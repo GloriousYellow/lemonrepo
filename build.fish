b203bfecc208960181b05d161bc8a338a237dac2
GloriousYellow <yellowglorious@gmail.com>
2019-01-16 18:31:32 -0500

    init

---
+++ build.fish
@@ -0,0 +1,17 @@
+#!/usr/bin/env fish
+
+rm -rf dist
+cp -r packages dist
+mkdir dist/_repo
+
+cd dist
+for dir in *
+  cd $dir
+  git init
+  env PKGEXT='.pkg.tar.xz' makepkg -s
+  cp *.pkg.tar.xz ../_repo
+  cd ..
+end
+
+cd _repo
+repo-add lemonrepo.db.tar.xz *.pkg.tar.xz
