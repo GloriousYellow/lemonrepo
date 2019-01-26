16be4cb503b2e1ea37fe778e3eddd73c61c7a075
GloriousYellow <yellowglorious@gmail.com>
2019-01-26 14:05:09 -0500

    e

--- build.fish
+++ build.fish
@@ -4,13 +4,17 @@ rm -rf dist
 cp -r packages dist
 mkdir dist/_repo
 
-chmod 777 -R .
+chmod 777 -Rv .
+
+useradd --no-create-home --shell=/bin/false build && usermod -L build
+echo "build ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "root ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 cd dist
 for dir in *
   cd $dir
   git init
-  su nobody -s /bin/sh -c "env PKGEXT='.pkg.tar.xz' makepkg -s"
+  su build -s /bin/sh -c "env PKGEXT='.pkg.tar.xz' makepkg -s"
   cp *.pkg.tar.xz ../_repo
   cd ..
 end
