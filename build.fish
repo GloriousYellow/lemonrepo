9a2a157e8fd0712ea62afd8668892d4f1fb05ea4
GloriousYellow <yellowglorious@gmail.com>
2019-01-27 17:59:42 -0500

    fsefsdf

--- build.fish
+++ build.fish
@@ -1,22 +1,22 @@
 #!/usr/bin/env fish
 
-rm -rf dist
-cp -r packages dist
-
 sed -i 's/exit \$E_ROOT/#disabled/g' /usr/sbin/makepkg
 
 git submodule update --init --recursive
+
+rm -rf dist
+cp -r packages dist
 
 cd dist
 for dir in *
   cd $dir
   git init
-  env PKGEXT='.pkg.tar.xz' makepkg -s
+  env PKGEXT='.pkg.tar.xz' makepkg -s --noconfirm
   cp *.pkg.tar.xz ../_repo
   cd ..
 end
 
-mkdir dist/_repo
+mkdir _repo
 
 cd _repo
 repo-add lemonrepo.db.tar.xz *.pkg.tar.xz
