68c857afe78337799de69fa81a0f461b609faa99
GloriousYellow <yellowglorious@gmail.com>
2019-02-11 09:15:25 -0500

    a

--- build.fish
+++ build.fish
@@ -14,7 +14,7 @@ cp -r packages dist
 
 cd dist
 
-#git clone git@github.com:GloriousYellow/lemonrepo-files.git _repo
+git clone git@github.com:GloriousYellow/lemonrepo-files.git _repo
 rm -rf _repo/*
 
 for dir in *
